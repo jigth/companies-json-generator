b83c7f4f65fd25d28708527da717db2bee4a7fa5
jigth <danielochoamontes@gmail.com>
2024-06-17 11:45:15 -0500

    FEAT: Improve companies generator to work with dynamic inputs and multiplatform

--- README.md
+++ README.md
@@ -6,8 +6,12 @@ _It generates a Million of random fake companies data by default in less than 10
 
 This number was chosen to avoid having RAM problems (getting out of RAM) in a 16GB RAM machine and get the result really fast
 
-## Example usage for Unix based Operating Systems (Linux-based, Mac, etc):
+## Example usage: 
+
+First execute the following command from a shell (replace the output path with an appropriate path)
 
 ```
-▶ go run src/main.go > /tmp/companies.json
+▶ go run src/main.go /output/path/goes/here/companies.json
 ```
+
+After that you'll be prompted for the num of companies to generate, enter that number and you'll have the companies generated in the output path you specified before
--- src/main.go
+++ src/main.go
@@ -2,15 +2,58 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"bufio"
+	"strings"
+	"strconv"
 	jsongenerator "github.com/jigth/companies-json-generator/src/json-generator"
 )
 
 func main() {
-	ONE_MILLION := 1000*1000
-	companies, err := jsongenerator.CreateFakeCompanies(ONE_MILLION)
+	outputPath := os.Args[1]
+
+	companiesAmount := readUserInput()
+
+	companies, err := jsongenerator.CreateFakeCompanies(companiesAmount)
 	if err != nil {
+		fmt.Println(err)
 		panic("error unmarshaling companies")
 	}
 
-	fmt.Println(companies)
+	file, err := os.Create(outputPath)
+	if err != nil {
+		fmt.Println(err)
+		panic("could not create the file in path " + outputPath)
+	}
+
+	_, err = fmt.Fprintf(file, "%v", companies)	
+	if err != nil {
+		fmt.Println(err)
+		panic("Could not write companies data in path " + outputPath)
+	}
+
+	fmt.Printf("%v Companies have been generated succesfully at %v\n", companiesAmount, outputPath)
+}
+
+func readUserInput() int {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter the number of fake companies data you want to generate")
+
+	companiesAmountStr, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		panic("error reading the input of companies to generate")
+	}
+
+	// Trim sufixes for Windows and Unix based systems
+	companiesAmountStr = strings.TrimSuffix(companiesAmountStr, "\n")
+	companiesAmountStr = strings.TrimSuffix(companiesAmountStr, "\r")
+
+	companiesAmount, err := strconv.Atoi(companiesAmountStr)
+	if err != nil {
+		fmt.Println(err)
+		panic("error reading the input of companies to generate")
+	}
+
+	return companiesAmount
 }
